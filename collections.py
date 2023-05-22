b00c2e65544ff913037d94048998b1a72dd71ebc
fafa2024 <134271829+fafa2024@users.noreply.github.com>
2023-05-22 09:57:41 +0100

    uploaded file

---
+++ collections.py
@@ -0,0 +1,27 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "orange"
+print(myFruitList)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple [0])
+print(myFinalAnswerTuple [1])
+print(myFinalAnswerTuple [2])
+
+myFavoriteFruitDictionary ={
+    "me" : "red",
+    "you" : "yellow",
+    "him" : "brown"
+    
+}
+print(myFavoriteFruitDictionary)
+print(myFavoriteFruitDictionary ["me"])
+print(myFavoriteFruitDictionary ["you"])
+print(myFavoriteFruitDictionary ["him"])
+print(type(myFavoriteFruitDictionary))
---
+++ sting-data-type.py
@@ -0,0 +1,6 @@
+
+color = input("What is your favorite color?  ")
+name = input("What is your name? ")
+print(name)
+animal = input("What is your favorite animal?  ")
+print("{}, your like a {] {}! "), format(color,name,animal)
